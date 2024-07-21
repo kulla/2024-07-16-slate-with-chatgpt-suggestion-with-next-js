7f7d3afcbc43ab563f4a5686b0cab264da1629d7
Stephan Kulla <git.mail@kulla.me>
2024-07-21 13:14:49 +0200

    style(page): Set max width to 720px

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const queryClient = new QueryClient()
 
 export default function Home() {
   return (
-    <main className="mx-auto w-[32em]">
+    <main className="mx-auto" style={{ maxWidth: '720px' }}>
       <QueryClientProvider client={queryClient}>
         <SlateEditor />
       </QueryClientProvider>
